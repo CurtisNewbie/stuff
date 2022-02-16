fe528d9a59800181c0180832d19c4a96a0a48121
yongjie.zhuang <yongj.zhuang@outlook.com>
2022-02-16 11:44:37 +0800

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -8,6 +8,10 @@ yellow=$'\e[1;33m'
 cyan=$'\e[1;36m'
 
 function gcmt() {
+    if [ -z $1 ]; then
+        echo_red "Please specify commit message"
+        exit 1
+    fi
     git add .
     git commit -m "$1"
 }
