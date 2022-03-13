7bdf08fe554b653d7688b1d13cb2665de288362c
CurtisNewbie <zhuangyongj@gmail.com>
2022-03-13 21:33:06 +0800

    Modified: build_sqlentitygenerator_jar.sh

--- build_sqlentitygenerator_jar.sh
+++ build_sqlentitygenerator_jar.sh
@@ -1,3 +1,5 @@
 javac SQLEntityGenerator.java
-jar cmf SQLEntityGenerator.mf SQLEntityGenerator.jar SQLEntityGenerator.class 'SQLEntityGenerator$1.class' 'SQLEntityGenerator$SQLTable.class' 'SQLEntityGenerator$SQLField.class' 
+# jar cmf SQLEntityGenerator.mf SQLEntityGenerator.jar SQLEntityGenerator.class 'SQLEntityGenerator$1.class' 'SQLEntityGenerator$SQLTable.class' 'SQLEntityGenerator$SQLField.class' 
+
+jar cmf SQLEntityGenerator.mf SQLEntityGenerator.jar *.class 
 rm *.class
