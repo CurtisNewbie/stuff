590a0e3739334993688abc64d6a41895733b83d7
yongj.zhuang <yongj.zhuang@outlook.com>
2022-03-16 00:11:01 +0800

    Modified: .gitignore Deleted: SQLEntityGenerator.jar Modified: build_sqlentitygenerator_jar.sh

--- build_sqlentitygenerator_jar.sh
+++ build_sqlentitygenerator_jar.sh
@@ -1,3 +1,3 @@
 javac SQLEntityGenerator.java
-jar cmf SQLEntityGenerator.mf SQLEntityGenerator.jar SQLEntityGenerator.class 'SQLEntityGenerator$1.class' 'SQLEntityGenerator$SQLTable.class' 'SQLEntityGenerator$SQLField.class' 
+jar cmf SQLEntityGenerator.mf SQLEntityGenerator.jar *.class 
 rm *.class
