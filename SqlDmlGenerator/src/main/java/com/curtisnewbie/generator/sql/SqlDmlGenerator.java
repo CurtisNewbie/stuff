0a6fe45340a641e81e3417c088be267ee5ede157
yongj.zhuang <yongj.zhuang@outlook.com>
2022-03-30 17:31:04 +0800

    Modified: SqlDmlGenerator/pom.xml Modified: SqlDmlGenerator/src/main/java/com/curtisnewbie/generator/sql/SqlDmlGenerator.java Modified: SqlDmlGenerator/src/test/java/com/curtisnewbie/generator/sql/SqlDmlGeneratorTest.java

--- SqlDmlGenerator/src/main/java/com/curtisnewbie/generator/sql/SqlDmlGenerator.java
+++ SqlDmlGenerator/src/main/java/com/curtisnewbie/generator/sql/SqlDmlGenerator.java
@@ -1,32 +1,118 @@
 package com.curtisnewbie.generator.sql;
 
+import com.fasterxml.jackson.core.type.*;
+import com.fasterxml.jackson.databind.*;
 import org.apache.commons.text.*;
 
+import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 /**
  * @author yongj.zhuang
  */
 public class SqlDmlGenerator {
 
+    private static final ObjectMapper om = new ObjectMapper();
+
     private static final String pre = "(";
     private static final String after = ")";
     private static final String delimiter = ",";
     private static final String last = ";";
 
-    private final String insertIntoValues;
-    private final List<Map<String, String>> params;
+    private final String dbName;
+    private final String tableName;
+    private final String fields;
+    private final List<Map<String, String>> params = new ArrayList<>();
+    private Map<String, String> defaultParam = new HashMap<>();
 
-    public SqlDmlGenerator(String insertIntoValues, List<Map<String, String>> params) {
-        this.insertIntoValues = insertIntoValues;
-        this.params = params != null ? params : new ArrayList<>();
+    /**
+     * Construct a SqlDmlGenerator
+     *
+     * @param fields list of fields delimited by comma
+     */
+    public SqlDmlGenerator(String fields, String dbName, String tableName) {
+        Objects.nonNull(fields);
+        this.fields = fields;
+        this.dbName = dbName;
+        this.tableName = tableName;
     }
 
+    /**
+     * Construct a SqlDmlGenerator
+     *
+     * @param fields list of fields delimited by comma
+     */
+    public SqlDmlGenerator(String fields, String tableName) {
+        Objects.nonNull(fields);
+        this.fields = fields;
+        this.dbName = null;
+        this.tableName = tableName;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param params parameters (each represent a row)
+     */
+    public SqlDmlGenerator withMapParams(List<Map<String, String>> params) {
+        Objects.nonNull(params);
+        this.params.addAll(params);
+        return this;
+    }
+
+    /**
+     * Default parameters
+     */
+    public SqlDmlGenerator withMapDefaultParam(Map<String, String> defaultParam) {
+        this.defaultParam.putAll(defaultParam);
+        return this;
+    }
+
+    /**
+     * Default parameters
+     */
+    public SqlDmlGenerator withJsonDefaultParam(String json) {
+        this.defaultParam.putAll(readAsMap(json));
+        return this;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param jsonArray array of json object (each represent a row)
+     */
+    public SqlDmlGenerator withJsonArrayParams(String jsonArray) {
+        Objects.nonNull(jsonArray);
+
+        this.params.addAll(readAsListOfMap(jsonArray));
+        return this;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param jsons list of json object (each represent a row)
+     */
+    public SqlDmlGenerator withJsonParams(List<String> jsons) {
+        Objects.nonNull(jsons);
+        this.params.addAll(jsons.stream()
+                .map(SqlDmlGenerator::readAsMap)
+                .collect(Collectors.toList()));
+        return this;
+    }
+
+    /**
+     * Generate (batch) insert SQL
+     */
     public void generateInsertSql() {
-        System.out.println(insertIntoValues);
-        final String template = parseTemplate(insertIntoValues);
+        final String dbNTable = dbName != null ? dbName + "." + tableName : tableName;
+        System.out.printf("INSERT INTO %s (%s) VALUES \n", dbNTable, fields);
+        final String template = genStrInterpolationTemplate(fields);
         for (int i = 0; i < params.size(); i++) {
-            final StrSubstitutor sub = new StrSubstitutor(params.get(i));
+            Map<String, String> m = new HashMap<>(defaultParam);
+            m.putAll(params.get(i));
+            final StrSubstitutor sub = new StrSubstitutor(m);
             String line = pre + sub.replace(template) + after;
             if (i < params.size() - 1)
                 line += delimiter;
@@ -36,17 +122,40 @@ public class SqlDmlGenerator {
         }
     }
 
-    private String parseTemplate(String insertInto) {
-        int l, h;
-        l = insertInto.indexOf("(");
-        h = insertInto.lastIndexOf(")");
-        final String[] fields = insertInto.substring(l + 1, h).split(",");
+    // ---------------------------------------------- helper methods -----------------------------------------
+
+    private static Map<String, String> readAsMap(String json) {
+        try {
+            return om.readValue(json, new TypeReference<Map<String, String>>() {
+            });
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static List<Map<String, String>> readAsListOfMap(String jsonArray) {
+        try {
+            return om.readValue(jsonArray, new TypeReference<List<Map<String, String>>>() {
+            });
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Generate a string interpolation template
+     * <p>
+     * Parse a list of fields delimited by ',' e.g., 'fieldA,fieldB,fieldC' into a string interpolation template: '${fieldA},${fieldB},${fieldC}'
+     * </p>
+     */
+    private static String genStrInterpolationTemplate(final String fields) {
+        final String[] fieldsArr = fields.split(",");
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < fields.length; i++) {
+        for (int i = 0; i < fieldsArr.length; i++) {
             if (i > 0) {
                 sb.append(",");
             }
-            sb.append("${").append(fields[i].trim()).append("}");
+            sb.append("${").append(fieldsArr[i].trim()).append("}");
         }
         return sb.toString();
     }
--- SqlDmlGenerator/src/test/java/com/curtisnewbie/generator/sql/SqlDmlGeneratorTest.java
+++ SqlDmlGenerator/src/test/java/com/curtisnewbie/generator/sql/SqlDmlGeneratorTest.java
@@ -1,6 +1,5 @@
 package com.curtisnewbie.generator.sql;
 
-import com.sun.net.httpserver.*;
 import org.junit.jupiter.api.*;
 
 import java.util.*;
@@ -10,75 +9,26 @@ import java.util.*;
  */
 public class SqlDmlGeneratorTest {
 
+    public static final String fields = "";
+    public static final String dbName = "";
+    public static final String tableName = "";
+    public static final String jsonDefaultParam = "";
+    public static final String jsonArrayParams = "";
+
     @Test
     public void generate_insert_template_sql() {
-
-        Map<String, String> defaultParams = new HashMap<>();
-        defaultParams.put("sender_type", "0");
-        defaultParams.put("title_en", "''");
-        defaultParams.put("content", "''");
-        defaultParams.put("content_en", "''");
-        defaultParams.put("business_id", "'ALL'");
-        defaultParams.put("name", "''");
-        defaultParams.put("status", "1");
-        defaultParams.put("created_at", "CURRENT_TIMESTAMP");
-        defaultParams.put("created_by", "''");
-        defaultParams.put("updated_by", "''");
-        defaultParams.put("updated_at", "CURRENT_TIMESTAMP");
-        defaultParams.put("trace_id", "''");
-        defaultParams.put("del_flag", "'N'");
-        final String insertIntoValues = "insert into message.t_msg_template(code,sender_type,title,title_en,content,content_en,business_id,name,description,status,created_by,created_at,updated_by,updated_at,trace_id,del_flag) values";
-        final List<Map<String, String>> params = Arrays.asList(
-                new ChainedMap(defaultParams).thenPut("code", "'SMS_237560619'").thenPut("title", "'VA 管理费收取成功提醒 (阿里云国内)'").thenPut("description", "'VA 管理费收取成功提醒 (阿里云国内)'").get(),
-                new ChainedMap(defaultParams).thenPut("code", "'SMS_237590493'").thenPut("title", "'VA 管理费收取成功提醒 (阿里云国际)'").thenPut("description", "'VA 管理费收取成功提醒 (阿里云国际)'").get(),
-                new ChainedMap(defaultParams).thenPut("code", "'SMS_237570578'").thenPut("title", "'VA 管理费收取失败提醒 (阿里云国内)'").thenPut("description", "'VA 管理费收取失败提醒 (阿里云国内)'").get(),
-                new ChainedMap(defaultParams).thenPut("code", "'SMS_237590496'").thenPut("title", "'VA 管理费收取失败提醒 (阿里云国际)'").thenPut("description", "'VA 管理费收取失败提醒 (阿里云国际)'").get(),
-                new ChainedMap(defaultParams).thenPut("code", "'50860'").thenPut("title", "'VA 管理费收取成功提醒 (祝通融)'").thenPut("description", "'VA 管理费收取成功提醒 (祝通融)'").get(),
-                new ChainedMap(defaultParams).thenPut("code", "'50861'").thenPut("title", "'VA 管理费收取失败提醒 (祝通融)'").thenPut("description", "'VA 管理费收取失败提醒 (祝通融)'").get(),
-                new ChainedMap(defaultParams).thenPut("sender_type", "3")
-                        .thenPut("code", "'va_mng_fee_charge_success'")
-                        .thenPut("title", "'VA 管理费收费成功'")
-                        .thenPut("title_en", "'VA Management Fee Charge Success'")
-                        .thenPut("content", "'尊敬的光子易会员${legalName}，您于${deductionTime}成功扣取账户管理费，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}，感谢您对PhotonPay支持！'")
-                        .thenPut("content_en", "'Dear PhotonPay member ${legalName}, ${deductionTime} successfully deducted the account management fee, the global account number is ${accountNumber}, the deduction amount is ${deductionAmtAndCurrency}, thank you for your support to PhotonPay!'")
-                        .thenPut("description", "'VA 管理费收费成功'").get(),
-
-                new ChainedMap(defaultParams).thenPut("sender_type", "3")
-                        .thenPut("code", "'va_mng_fee_charge_failure'")
-                        .thenPut("title", "'VA 管理费收费失败'")
-                        .thenPut("title_en", "'VA Management Fee Charge Failure'")
-                        .thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}因账户余额不足，账户管理费扣款失败，全球账户是${accountNumber}，扣费金额为${deductionAmtAndCurrency}。该账户现进入待注销期（即日起至${lastDay}），超过日期仍没有扣费成功，则账户会被注销，如需帮助请联系我们。400-859-9576'")
-                        .thenPut("content_en", "'Dear PhotonEase member ${legalName}, ${deductionTime} account management fee debit failed due to insufficient account balance, global account is ${accountNumber}, debit amount is ${deductionAmtAndCurrency}. The account is now in the pending cancellation period (from now until ${lastDay}), after the date there is still no successful deduction, the account will be cancelled, if you need help please contact us. 400-859-9576'")
-                        .thenPut("description", "'VA 管理费收费失败'").get()
-        );
-
-        SqlDmlGenerator sqlDmlGenerator = new SqlDmlGenerator(insertIntoValues, params);
-        sqlDmlGenerator.generateInsertSql();
+        new SqlDmlGenerator(fields, dbName, tableName)
+                .withMapDefaultParam(new ChainedMap()
+                        .thenPut("created_at", "CURRENT_TIMESTAMP")
+                        .thenPut("created_by", "''")
+                        .thenPut("updated_by", "''")
+                        .thenPut("updated_at", "CURRENT_TIMESTAMP")
+                        .thenPut("trace_id", "''")
+                        .thenPut("del_flag", "'N'")
+                        .get())
+                .withJsonDefaultParam(jsonDefaultParam)
+                .withJsonArrayParams(jsonArrayParams)
+                .generateInsertSql();
     }
 
-    @Test
-    public void generate_insert_sender_template_sql() {
-
-        Map<String, String> defaultParams = new HashMap<>();
-        defaultParams.put("remark", "''");
-        defaultParams.put("created_at", "CURRENT_TIMESTAMP");
-        defaultParams.put("created_by", "''");
-        defaultParams.put("updated_by", "''");
-        defaultParams.put("updated_at", "CURRENT_TIMESTAMP");
-        defaultParams.put("trace_id", "''");
-        defaultParams.put("del_flag", "'N'");
-
-        final String insertIntoValues = "INSERT INTO message_dev.t_msg_sender_template (template_code,sender_code,sender_template_code,content,remark,created_by,created_at,updated_by,updated_at,trace_id,del_flag) values";
-        final List<Map<String, String>> params = Arrays.asList(
-                new ChainedMap(defaultParams).thenPut("template_code", "'SMS_237560619'").thenPut("sender_code", "'aliSms'").thenPut("sender_template_code", "'SMS_237560619'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}成功扣取账户管理费，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}，扣费详情请登录光子易会员端！'").get(),
-                new ChainedMap(defaultParams).thenPut("template_code", "'SMS_237590493'").thenPut("sender_code", "'aliSms'").thenPut("sender_template_code", "'SMS_237590493'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}成功扣取账户管理费，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}，扣费详情请登录光子易会员端！'").get(),
-                new ChainedMap(defaultParams).thenPut("template_code", "'SMS_237570578'").thenPut("sender_code", "'aliSms'").thenPut("sender_template_code", "'SMS_237570578'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}因账户余额不足，账户管理费扣款失败，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}。该账户现进入待注销期（即日起至${lastDay}），超过日期仍没有扣费成功，则账户会被注销，如需帮助请联系我们。400-859-9576'").get(),
-                new ChainedMap(defaultParams).thenPut("template_code", "'SMS_237590496'").thenPut("sender_code", "'aliSms'").thenPut("sender_template_code", "'SMS_237590496'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}因账户余额不足，账户管理费扣款失败，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}。该账户现进入待注销期（即日起至${lastDay}），超过日期仍没有扣费成功，则账户会被注销，如需帮助请联系我们。400-859-9576'").get(),
-                new ChainedMap(defaultParams).thenPut("template_code", "'50860'").thenPut("sender_code", "'ztSms'").thenPut("sender_template_code", "'50860'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}成功扣取账户管理费，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}，扣费详情请登录光子易会员端！'").get(),
-                new ChainedMap(defaultParams).thenPut("template_code", "'50861'").thenPut("sender_code", "'ztSms'").thenPut("sender_template_code", "'50861'").thenPut("content", "'尊敬的光子易会员${legalName}，${deductionTime}因账户余额不足，账户管理费扣款失败，全球账号是${accountNumber}，扣费金额为${deductionAmtAndCurrency}。该账户现进入待注销期（即日起至${lastDay}），超过日期仍没有扣费成功，则账户会被注销，如需帮助请联系我们。400-859-9576'").get()
-        );
-
-        SqlDmlGenerator sqlDmlGenerator = new SqlDmlGenerator(insertIntoValues, params);
-        sqlDmlGenerator.generateInsertSql();
-    }
 }
