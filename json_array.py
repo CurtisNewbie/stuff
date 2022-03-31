53943e80c2caf1a63a3e6f8ebefd3425ae432add
yongj.zhuang <zhuangyongj@gmail.com>
2022-03-31 23:43:42 +0800

    Added: json_array.py

---
+++ json_array.py
@@ -0,0 +1,26 @@
+import sys
+
+def json_arr(text):
+    arr = text.split(",")
+    joined = '['
+
+    for i, v in enumerate(arr):
+        if i > 0:
+            joined += ',';
+        joined += '"' + v.strip() + '"'
+
+    joined += ']'
+    print(joined)
+
+
+def print_help():
+    print('arg[0] - comma delimited strings to be joined as a json array')
+
+if __name__ == "__main__"   :
+    if len(sys.argv) != 2: 
+        print_help()
+        raise Exception('Illegal Arguments')  
+
+    json_arr(sys.argv[1])
+
+
