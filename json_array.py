b155ddd79036d542383c297742019915d3329f7d
yongj.zhuang <yongj.zhuang@outlook.com>
2022-04-01 13:12:07 +0800

    Modified: json_array.py

--- json_array.py
+++ json_array.py
@@ -17,7 +17,7 @@ def print_help():
     print('arg[0] - comma delimited strings to be joined as a json array')
 
 if __name__ == "__main__"   :
-    if len(sys.argv) != 2: 
+    if len(sys.argv) < 2: 
         print_help()
         raise Exception('Illegal Arguments')  
 
