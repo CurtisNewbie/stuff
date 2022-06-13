e670de84742b97754c91119bb7fbc2938292eef7
yongj.zhuang <zhuangyongj@gmail.com>
2022-06-13 23:08:45 +0800

    Added: enable_go_cn_proxy.sh

---
+++ enable_go_cn_proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+go env -w GO111MODULE=on
+
+# go env -w  GOPROXY=https://goproxy.cn,direct
+# go env -w  GOPROXY=https://goproxy.io,direct
+
+go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
+
+
+### ---- unset
+# go env -u GO111MODULE
+# go env -u GOPROXY
+### ---------
