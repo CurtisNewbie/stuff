e910caaae859327fdbe02aa495861b202d5b2d41
yongj.zhuang <yongj.zhuang@outlook.com>
2022-08-06 18:24:56 +0800

    insertgen.py but with pandas

---
+++ insertgenpy/insertgenpd.py
@@ -0,0 +1,166 @@
+from genericpath import exists
+import sys
+import os
+import pandas
+
+debug = False
+mute = False
+flags = {"--debug", "--mute"}
+keywords: list[str] = ["CURRENT_TIMESTAMP", "NOW()"]
+env_default_values_key = "INSERTGEN_DEFAULT"
+
+
+# check if w is a sql keyword
+def issqlkeyword(w: any) -> bool:
+    lw = str(w).lower()
+    for i in range(len(keywords)):
+        if keywords[i].lower() == lw:
+            return True
+    return False
+
+
+def escape(w: str) -> str:
+    if w == "\"\"":
+        return w
+    return w if issqlkeyword(w) else f"\"{w}\""
+
+
+'''
+python3 -m pip install pandas 
+
+[0] - input path
+[1] - database/table name
+[2] - output path
+
+'--debug' for debug mode
+'--mute' for not printing the generated sql on console
+
+Environmnet variable:
+
+'INSERTGEN_DEFAULT=...' specify the default column and value
+
+Yongj.Zhuang
+'''
+if __name__ == '__main__':
+
+    la = len(sys.argv)
+    if la < 2:
+        print("\n insertgenpd.py (pandas) by Yongj.Zhuang")
+        print("\n Please provide following arguments:\n")
+        print(" [0] - input path")
+        print(" [1] - database/table name")
+        print(" [2] - output path (optional)")
+        print("\n e.g., python3 insertgen.py myexcel.xls generated.sql mytable")
+        print("\n '--debug' for debug mode")
+        print("\n '--mute' for not printing the generated sql on console")
+        print("\n 'INSERTGEN_DEFAULT' environment variable for sepcifying the default columns and values")
+        print(
+            "\n e.g., INSERTGEN_DEFAULT=\"created_at=CURRENT_TIMESTAMP,created_by=SYSTEM\"")
+        print()
+        sys.exit(1)
+
+    ip = sys.argv[1]
+    tb = sys.argv[2]
+    op = sys.argv[3] if la > 3 and sys.argv[3] not in flags else None
+
+    for i in range(2, la):
+        v = sys.argv[i]
+        if v == "--debug":
+            debug = True
+        elif v == '--mute':
+            mute = True
+
+    # parse default columns and values
+    defaultenv = os.environ.get(env_default_values_key)
+    defaultk = []
+    defaultv = []
+    if defaultenv is not None:
+        tokens = defaultenv.split(",")
+        for i in range(len(tokens)):
+            ts = tokens[i].split("=")
+            defaultk.append(ts[0])
+            defaultv.append("\"\"" if len(ts) < 1 else escape(ts[1]))
+
+    if debug:
+        print(
+            f"[debug] default env: '{defaultenv}', columns: {defaultk}, values: {defaultv}")
+        print(
+            f"[debug] input path: '{ip}', output path: '{op}', table name: '{tb}'")
+
+    # read and parse workbook
+    if not exists(ip):
+        print(f"Input file '{ip}' not found")
+        sys.exit(1)
+
+    df: pandas.DataFrame = pandas.read_excel(ip, 0)
+    if debug:
+        print(f"[debug] opened workbook '{ip}', content: '{df}'")
+
+    nrow = len(df)
+    ncol = len(df.columns) 
+    if debug:
+        print(f"[debug] row count: {nrow}, col count: {ncol}")
+
+    ncolmap = {}
+
+    # parse header
+    headers = []
+    for i in range(ncol):
+        h = df.iat[0, i]
+        if h:
+            headers.append(h)
+    for i in range(len(defaultk)):
+        headers.append(defaultk[i])
+    nheaders = len(headers)
+
+    if debug:
+        print(f"[debug] headers: {headers}")
+
+    body: list[list[str]] = []
+    # parse body
+    for i in range(1, nrow):
+        r = []
+        for j in range(ncol):
+            r.append(df.iat[i, j])
+        for k in range(len(defaultv)):
+            r.append(defaultv[k])
+        body.append(r)
+
+    if debug:
+        print(f"[debug] body: {body}")
+
+    # -------
+    #
+    # starts generating the insert sql
+    #
+    # -------
+
+    insert = "INSERT INTO "
+
+    if tb is not None:
+        insert = insert + tb
+
+    insert += "(" + ",".join(headers) + ") VALUES "
+
+    lbody = len(body)
+    for i in range(lbody):
+        row = body[i]
+        preprocessed = []
+
+        for j in range(nheaders):
+            w = escape(row[j])
+            preprocessed.append(w)
+
+        insert += "\n  (" + ",".join(preprocessed) + ")"
+        if i < lbody - 1:
+            insert += ","
+
+    insert += ";"
+
+    if not mute:
+        print(insert)
+
+    if op is not None:
+        f = open(op, "w")
+        f.write(insert)
+        f.close()
