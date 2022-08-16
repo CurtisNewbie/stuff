ca1227b57bec83e2e25d6ec4f5f2defd2972395d
yongj.zhuang <zhuangyongj@gmail.com>
2022-08-16 21:02:53 +0800

    Modified: randp.py

--- randp.py
+++ randp.py
@@ -1,6 +1,8 @@
 import random
 import sys
 
+DEFAULT_LEN = 25
+
 
 def buildcharset() -> str:
     s = ""
@@ -15,7 +17,7 @@ def buildcharset() -> str:
 
 if __name__ == '__main__':
     letters = buildcharset()
-    length = 20
+    length = DEFAULT_LEN 
 
     if len(sys.argv) > 1: 
         length = int(sys.argv[1])
