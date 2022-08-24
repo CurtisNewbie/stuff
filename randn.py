744a65acac456b421b7d4ba588be4331f0a5607f
yongj.zhuang <yongj.zhuang@outlook.com>
2022-08-24 11:35:18 +0800

    Added: randn.py : 00	randp.py	rands.py

---
+++ randn.py
@@ -0,0 +1,21 @@
+import random
+import sys
+
+DEFAULT_LEN = 25
+
+
+def buildcharset() -> str:
+    s = ""
+    for i in range(10):
+        s = s + chr(i + ord('0'))
+    return s
+
+
+if __name__ == '__main__':
+    letters = buildcharset()
+    length = DEFAULT_LEN
+
+    if len(sys.argv) > 1:
+        length = int(sys.argv[1])
+
+    print(''.join(random.choice(letters) for i in range(length)))
