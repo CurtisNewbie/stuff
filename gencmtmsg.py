c70aa49de3f343c9c81f66ed0945315626c510fc
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-07 14:51:59 +0800

    Modified:  gencmtmsg.py

--- gencmtmsg.py
+++ gencmtmsg.py
@@ -15,6 +15,17 @@ def translate_flag(flag: str) -> str:
     return t
 
 
+def parse_flag(s: str) -> tuple[str, int]:
+    j = -1
+    for i in range(4):
+        j = s.find(" ", j + 1)
+        # print(f"j: {j}")
+    if j < 0:
+        raise ValueError(f"Failed to parse flag, s: {s}")
+
+    return s[j+1], j+2
+
+
 if __name__ == '__main__':
     with subprocess.Popen(
             f"git diff --staged --raw", shell=True, stdout=subprocess.PIPE) as p:
@@ -32,13 +43,13 @@ if __name__ == '__main__':
         sp = diff.splitlines()
         for i in range(len(sp)):
             line = sp[i]
-            flag = line[31:32]
+            flag, si = parse_flag(line)
             # print(flag)
 
             # renamed a file
             # print(f"flag: {flag}")
             if flag.upper() == "R":
-                file = line[32:]
+                file = line[si:]
                 # print(f"f0: {file}")
                 for j in range(len(file)):
                     if file[j] == "\t":
@@ -51,7 +62,7 @@ if __name__ == '__main__':
                 if k > -1:
                     file = file[:k]
             else:
-                file = line[32:].strip()
+                file = line[si:].strip()
             # print(f"f2: {file}")
 
             translated = translate_flag(flag) + ":"
