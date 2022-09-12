1986c7a8ca38a14ac3945e9e5457ce0b34620d3b
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-12 23:54:23 +0800

    Modified:  tfields.py

--- tfields.py
+++ tfields.py
@@ -1,24 +1,29 @@
+import argparse
 import mysql.connector
 import mysql.connector.cursor
 import sys
 
-flag: set[str] = {'-excl-default'}
 isdebug = False
 
 
-def isflag(s: str):
-    return s in flag
-
-
-def printhelp():
-    print("\ntfields.py by Yongj.Zhuang\n")
-    print(" -user username")
-    print(" -password password")
-    print(" -host host (by default it's localhost)")
-    print(" -database database name (optional if table name includes the database)")
-    print(" -table table name")
-    print(" -excl-default exclude columns with default value")
-    print("\n e.g., python3 tfields.py -user root -password '123456' -host 192.168.10.1 -table mydb.mytable -excl-default\n")
+def parsearg():
+    ap = argparse.ArgumentParser("tfields.py by Yongj.Zhuang")
+    ap.add_argument("-user", help="username", type=str, required=True)
+    ap.add_argument("-password", help="password",
+                    type=str, default="", required=False)
+    ap.add_argument(
+        "-host", help="host (by default it's localhost)", type=str, default="localhost", required=False)
+    ap.add_argument("-database",
+                    help="database, optional if it's already included in the table name", type=str,  default="", required=False)
+    ap.add_argument(
+        "-table", help="table name (may include database name)")
+    ap.add_argument(
+        "-excldefault", help="exclude columns with default value", action="store_true", required=False)
+    ap.add_argument(
+        "-exclcol", help="exclude columns, delimited by comma", default="", required=False)
+    ap.add_argument(
+        "-debug", help="debug mode", action="store_true", required=False)
+    return ap.parse_args()
 
 
 def debug(callback):
@@ -26,48 +31,14 @@ def debug(callback):
         print(callback())
 
 
-def getdefault(argdict, key, default):
-    return argdict[key] if key in argdict else default
-
-
-def getrequired(argdict, key):
-    if key in argdict:
-        return argdict[key]
-    print(f"Missing argument: '{key}'")
-    sys.exit(1)
-
-
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    nargs = len(args)
-    if nargs < 1:
-        printhelp()
-        sys.exit(1)
 
-    debug(lambda: f"args: {args}")
+    args = parsearg()
+    isdebug = args.debug
 
+    debug(lambda: f"args: {sys.argv}, parsed: {args}")
     argdict: dict[str, str] = {}
-
-    i = 0
-    while i < nargs:
-        k = str(args[i])
-        if isflag(k):
-            argdict[k] = True
-            i += 1
-            continue
-
-        if i + 1 >= nargs:
-            debug(lambda: f"missing arguments, i: {i}, nargs: {nargs}")
-            print(f"missing value after argument: '{k}'")
-            sys.exit(1)
-
-        va = args[i + 1]
-        argdict[k] = str(va)
-        i += 2
-
-    debug(lambda: f"argdict: {argdict}")
-
-    table: str = getrequired(argdict, "-table")
+    table: str = args.table
     database = None
 
     j = table.find('.')
@@ -77,15 +48,15 @@ if __name__ == '__main__':
         table = table[j + 1:]
 
     if database is None:
-        database = getrequired(argdict, "-database")
+        database = args.database
 
     config = {
-        'user': getrequired(argdict, "-user"),
-        'password': getrequired(argdict, "-password"),
-        'host': getdefault(argdict, "-host", 'localhost'),
+        'user': args.user,
+        'password': args.password,
+        'host': args.host,
         'database': database,
         'raise_on_warnings': True,
-        'connect_timeout': 1000
+        'connect_timeout': 5000
     }
     debug(lambda: f"config: {config}")
 
@@ -112,15 +83,26 @@ if __name__ == '__main__':
             exclidx = i
             break
 
-    excldef = getdefault(argdict, '-excl-default', False) and exclidx > -1
+    debug(lambda: f"args.excldefault: {args.excldefault}")
+    excldef = args.excldefault and exclidx > -1
     debug(lambda: f"excluding column with default values")
+
+    excluded: set[str] = set()
+    if args.exclcol != "":
+        cols: list[str] = str(args.exclcol).split(",")
+        excluded = set(cols)
+    debug(lambda: f"columns specified in -exclcol {excluded}")
 
     fields = []
     for i in range(len(resultset)):
         n = resultset[i][0]
-        dv = resultset[i][exclidx]
-        if excldef and dv != None:
-            debug(lambda: f"column '{n}' has default value '{dv}', skipped")
+        defaultval = resultset[i][exclidx] # column for default value
+        if excldef and defaultval != None:
+            debug(
+                lambda: f"column '{n}' has default value '{defaultval}', skipped")
+            continue
+        if n in excluded:
+            debug(lambda: f"column '{n}' is excluded")
             continue
         fields.append(n)
 
