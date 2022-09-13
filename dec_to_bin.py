beda47af13da933ee68d3541842a1d50dd9bef4e
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-13 10:11:31 +0800

    Added:     dec_to_bin.py

---
+++ dec_to_bin.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import sys
+
+num_lower_bound = ord('0')
+num_upper_bound = ord('9')
+alpha_lower_bound = ord('a')
+
+
+def to_hex(n: int) -> str:
+    if n < 10:
+        c = chr(num_lower_bound + n)
+    else:
+        c = chr(alpha_lower_bound + n - 10)
+    return c
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+
+    num = int(args[0])
+    n = num
+
+    s = ''
+    while num > 0:
+        rem = num % 2
+        s = to_hex(rem) + s
+        num = int(num / 2)
+
+    print(f"dec: \"{n}\" to bin: \"{s}\"")
