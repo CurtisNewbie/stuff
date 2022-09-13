baadcc9cfd47263fb9b237849f91ce7975736aa9
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-13 10:20:07 +0800

    Modified:  hex_to_dec.py

--- hex_to_dec.py
+++ hex_to_dec.py
@@ -7,27 +7,28 @@ num_lower_bound = ord('0')
 num_upper_bound = ord('9')
 alpha_lower_bound = ord('a')
 
+
 def to_int(s: str) -> int:
     c = s.lower()[0]
     code = ord(c)
     if code < num_upper_bound:
         return code - num_lower_bound
     else:
-        return code - alpha_lower_bound + 10 
+        return code - alpha_lower_bound + 10
+
 
 if __name__ == '__main__':
     args = sys.argv[1:]
     hx = args[0]
+    if hx.startswith("0x"):
+        hx = hx[2:]
+
     hl = len(hx)
 
     s = 0
     for i in range(hl):
-        rev_i = hl - i - 1 
+        rev_i = hl - i - 1
         pv = math.pow(16, rev_i)
-        s += to_int(hx[i]) * pv 
+        s += to_int(hx[i]) * pv
 
     print(f"hex: \"{hx}\" to dec: \"{int(s)}\"")
-
-
-
-
