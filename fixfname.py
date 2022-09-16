5195631c0e47b2fc34bf0dc5ea3f6db1f203c2bf
yongj.zhuang <yongj.zhuang@outlook.com>
2022-09-16 18:06:36 +0800

    Modified:  fixfname.py

--- fixfname.py
+++ fixfname.py
@@ -9,15 +9,17 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         sys.exit(0)
 
-    f = sys.argv[1]
-    if not exists(f):
-        print(f"file {f} not found")
-        sys.exit(1)
+    l = sys.argv[1:]
+    for i in range(len(l)):
+        f = l[i]
+        if not exists(f):
+            print(f"file {f} not found")
+            sys.exit(1)
 
-    ff = re.sub("[ \\-）（,\'+]", "_", f.strip())
-    ff = re.sub("_+", "_", ff)
-    if f == ff:
-        sys.exit(0)
+        ff = re.sub("[ \\-）（,\'+]", "_", f.strip())
+        ff = re.sub("_+", "_", ff)
+        if f == ff:
+            sys.exit(0)
 
-    os.rename(f, ff)
-    print(f"renamed from '{f}' to '{ff}'")
+        os.rename(f, ff)
+        print(f"renamed from '{f}' to '{ff}'")
