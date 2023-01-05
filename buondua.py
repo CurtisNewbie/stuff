a139b41b17104fbd59f8b47c8f4dc96f8e570119
yongj.zhuang <yongj.zhuang@outlook.com>
2023-01-05 11:29:36 +0800

    Improve buondua.py

--- buondua.py
+++ buondua.py
@@ -22,14 +22,178 @@ Yongj.zhuang
 #    'https': 'http://localhost:7890',
 # }
 
-
-max_retry = 5 
+mock = False
 render_timeout = 60
 down_timeout = 10
 seg = ['kul.mrcong.com', 'buondua.art']
 base = 'https://buondua.com'
-parsed = 'parse_url.txt'
 
+'''
+Format of the input file:
+
+[sites that haven't been pre-processed]
+[sites that haven't been pre-processed]
+[sites that haven't been pre-processed]
+
+--- PRE_PROCESSED ---
+
+[sites that have been pre-processed]
+[sites that have been pre-processed]
+[sites that have been pre-processed]
+
+--- PARSED ---
+
+[sites that have been parsed]
+[sites that have been parsed]
+[sites that have been parsed]
+
+--- EXTRACTED ---
+
+[img urls extracted]
+[img urls extracted]
+[img urls extracted]
+
+--- DOWNLOADED ---
+
+[img urls downloaded]
+[img urls downloaded]
+[img urls downloaded]
+
+--- END ---
+
+'''
+PRE_PROCESSED_LINE = "--- PRE_PROCESSED ---"
+PARSED_LINE = "--- PARSED ---"
+EXTRACTED_LINE = "--- EXTRACTED ---"
+DOWNLOADED_LINE = "--- DOWNLOADED ---"
+END_LINE = "--- END ---"
+
+STAGE_PRISTINE = 0
+STAGE_PREPROCESSED = 1
+STAGE_PARSED = 2
+STAGE_EXTRACTED = 3
+STAGE_DOWNLOAED = 4
+
+class Context:
+    
+    def __init__(self, file: str):
+        self.file = file
+
+
+    def _reset(self):
+        self.pristine = set() # website url
+        self.preprocessed = set() # website url
+        self.parsed = set()  # website url
+        self.extracted = set() # image url
+        self.downloaded = set() # image url
+
+    def __str__(self):
+        s = "Context:"
+        s += f'\n   pristine: {self.pristine}'
+        s += f'\n   preprocessed: {self.preprocessed}'
+        s += f'\n   parsed: {self.parsed}'
+        s += f'\n   extracted (img): {self.pristine}'
+        s += f'\n   downloaded (img): {self.downloaded}'
+        return s
+
+    def rec_preprocessed(self, site: str):
+        self.pristine.remove(site)
+        self.preprocessed.add(site)
+
+    def rec_parsed(self, site: str):
+        self.preprocessed.remove(site)
+        self.parsed.add(site)
+        
+    def rec_extracted(self, img_urls: list[str]):
+        for u in img_urls: 
+            self.extracted.add(u)
+
+    def rec_downloaded(self, img_url: str):
+        self.extracted.remove(img_url)
+        self.downloaded.add(img_url)
+    
+    def persist(self): 
+        content = ""
+
+        if self.pristine:
+            content += "\n" + "\n".join(self.pristine)
+        content += "\n" + PRE_PROCESSED_LINE
+
+        if self.preprocessed:
+            content += "\n" + "\n".join(self.preprocessed)
+        content += "\n" + PARSED_LINE 
+
+        if self.parsed:
+            content += "\n" + "\n".join(self.parsed)
+        content += "\n" + EXTRACTED_LINE 
+
+        if self.extracted:
+            content += "\n" + "\n".join(self.extracted)
+        content += "\n" + DOWNLOADED_LINE 
+
+        if self.downloaded:
+            content += "\n" + "\n".join(self.downloaded)
+        content += "\n" + END_LINE 
+
+        with open(self.file, "w") as f:
+            f.write(content)
+
+
+    def load(self):
+        self._reset()
+
+        with open(self.file, "r") as f:
+            lines = list(filter(lambda l : l, map(lambda x: x.replace("\n", "").strip(), f.readlines())))
+
+            reset_curr = False
+            curr = set() 
+            stage = STAGE_PRISTINE 
+
+            for l in lines:
+                if l == PRE_PROCESSED_LINE:
+                    reset_curr = True
+                    self.pristine = curr
+                elif l == PARSED_LINE:
+                    reset_curr = True
+                    self.preprocessed = curr
+                elif l == EXTRACTED_LINE:
+                    reset_curr = True
+                    self.parsed = curr
+                elif l == DOWNLOADED_LINE: 
+                    reset_curr = True
+                    self.extracted = curr
+                elif l == END_LINE: 
+                    reset_curr = True
+                    self.downloaded = curr
+                else:
+                    curr.add(l)
+                
+                if reset_curr:
+                    reset_curr = False
+                    curr = set()
+                    stage = stage + 1
+
+                    if stage > STAGE_DOWNLOAED + 1:
+                        break
+
+            if len(curr) > 0:
+                if stage == STAGE_PRISTINE:
+                    self.pristine = curr
+                elif stage == STAGE_PREPROCESSED:
+                    self.preprocessing = curr
+                elif stage == STAGE_PARSED:
+                    self.parsed = curr
+                elif stage == STAGE_EXTRACTED:
+                    self.extracted = curr
+                elif stage == STAGE_DOWNLOAED:
+                    self.downloaded = curr            
+
+    def is_finished(self) -> bool:
+        if len(self.pristine) > 0 or len(self.preprocessed) > 0:
+            return False 
+        if len(self.parsed) > 0 or len(self.extracted) > 0:
+            return False
+        return True 
 
 def render_html(url: str) -> str:
     # session = HTMLSession(browser_args=[f"--proxy-server={proxy_server}"])
@@ -41,77 +205,77 @@ def render_html(url: str) -> str:
     return r.html.html
 
 
-def filter_url(url: str) -> bool:
+def filter_img_url(url: str) -> bool:
     for s in seg:
         if url.find(s) > -1: return True
     return False
 
 
-def parse_urls(urls: list[str], output_file: str, saveUrlFunc): 
-    parsed = set()
+def parse_pagination(url) -> list[str]:
+    renderstart = time.time()
+    html = render_html(url) 
+    print(f"Rendered HTML '{url}' (took {(time.time() - renderstart):.3}s)")
 
-    with open(output_file, "w") as f:
-        i = 0
-        while i < len(urls):
-            url = urls[i] 
-            if url in parsed:
-                i = i + 1
+    expanded = [url]
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # it may have several pages (div -> span -> a.href) 
+    '''
+    <nav class="pagination">
+        <div class="pagination-list">
+            <span> <a class="pagination-link is-current" href="..."> 1 </a> </span>
+            <span> <a class="pagination-link " href="..."> 2 </a> </span>
+        </div>
+    </nav>
+    '''
+
+    for pdiv in soup.find_all('div', {"class": "pagination-list"}):
+        for pspan in pdiv.descendants:
+            if not isinstance(pspan, bs4.element.Tag):
                 continue
+            for atag in pspan.descendants:
+                if not isinstance(atag, bs4.element.Tag):
+                    continue
+                href = atag.get('href')
+                if not href: 
+                    continue
+                if not href.lower().startswith('http'): 
+                    href = base + "/" + urllib.parse.quote(href[1:], safe="")
+                expanded.append(href)
 
-            print(f"Fetching HTML for '{url}'")
-            t = len(urls)
+    return expanded
 
-            retry = max_retry
-            while True:
-                try:
-                    renderstart = time.time()
-                    html = render_html(url) 
-                    print(f"Rendered HTML '{url}' (took {(time.time() - renderstart):.3}s)")
-                    # print(html)
 
-                    extracted = []
-                    soup = BeautifulSoup(html, 'html.parser')
-                    for img in soup.find_all('img'):
-                        src: str = img.get('src')
+def extract_img_urls(ctx: Context): 
+    print("Start extracting image urls from websites ...")
 
-                        if not filter_url(src): continue
-                        extracted.append(src)
-                    
-                    # it may have several pages (div -> span -> a.href) 
-                    '''
-                    <nav class="pagination">
-                        <div class="pagination-list">
-                            <span> <a class="pagination-link is-current" href="..."> 1 </a> </span>
-                            <span> <a class="pagination-link " href="..."> 2 </a> </span>
-                        </div>
-                    </nav>
-                    '''
+    preprocessed = set(ctx.preprocessed)
+    total = len(preprocessed)
+    i = 0
+    for url in preprocessed: 
+        i += 1
+        progress = f"[{i+1}/{total}]"
 
-                    for pdiv in soup.find_all('div', {"class": "pagination-list"}):
-                        for pspan in pdiv.descendants:
-                            if isinstance(pspan, bs4.element.Tag):
-                                for atag in pspan.descendants:
-                                    if isinstance(atag, bs4.element.Tag):
-                                        href = atag.get('href')
-                                        if not href: continue
-                                        if not href.lower().startswith('http'): 
-                                            href = base + "/" + urllib.parse.quote(href[1:], safe="")
-                                        if href not in urls: urls.append(href)
+        print(f"Fetching HTML for '{url}'")
+        try:
+            renderstart = time.time()
+            html = render_html(url) 
+            print(f"Rendered HTML '{url}' (took {(time.time() - renderstart):.3}s)")
 
-                    print(f"[{i+1}/{t}] Parsed '{url}', found: {len(extracted)}")
-                    f.write(f"# [{i+1}/{t}] {url}, count: {len(extracted)}\n")
-                    f.write("\n".join(extracted) + "\n\n")
+            extracted = []
+            soup = BeautifulSoup(html, 'html.parser')
+            for img in soup.find_all('img'):
+                src: str = img.get('src')
 
-                    if saveUrlFunc: saveUrlFunc(urls)
-                    i = i + 1
-                    parsed.add(url)
-                    break 
-                except Exception as e:
-                    retry = retry - 1
-                    print(f"[{i+1}/{t}] Failed to parse url '{url}', e: {e}, retry remaining: {retry}")
-                    if retry == 0:
-                        raise e
-                    time.sleep(1)
+                if not filter_img_url(src): continue
+                extracted.append(src)
+            print(f"{progress} Parsed '{url}', found {len(extracted)} image urls")
+
+            ctx.rec_extracted(extracted)
+            ctx.rec_parsed()
+
+        except Exception as e:
+            print(f"{progress} Failed to parse url '{url}': {e}")
 
 
 suf = ['.webp', '.jpg', '.jpeg']
@@ -124,52 +288,39 @@ def extract_name(url: str) -> str:
         if j < 0: continue
 
         i = url.rfind('/', 0, j)
-        # print(f"url: {url}, i: {i}, j: {j}")
         return url[i+1:j+len(s)] 
 
     raise ValueError(f'Failed to extract filename for "{url}"')
 
 
-def download(input_file: str, target_dir: str) -> bool:
-    print(f"opening file: {input_file}\n")
+def download(ctx: Context, target_dir: str):
+    print("Start downloading ...")
 
-    failed = []
-    with open(input_file) as f:
-        all = f.read().replace("\t", "\n")
-        lines = all.splitlines()
+    extracted: set[str] = set(ctx.extracted)
+    total = len(extracted)
+    i = 0
 
-        dcnt = 0
-        t = len(lines)
-        for i in range(t):
-            url = lines[i]
-            if not url: continue
-            if url.startswith("#"): continue
+    for img_url in extracted: 
+        i += 1
+        filename = target_dir + extract_name(img_url)
+        progress = f"[{i}/{total}]"
 
-            filename = target_dir + extract_name(url)
-            if os.path.exists(filename): 
-                print(f"[{i+1}/{t}] File '{filename}' already downloaded")
-                continue
-            dcnt = dcnt + 1
+        if os.path.exists(filename): 
+            print(f"{progress} File '{filename}' already downloaded")
+            continue
 
-            try:
-                response = requests.get(url, timeout=down_timeout)
+        try:
+            if mock:
+                print(f"{progress} Downloaded (mocked) '{img_url}' as '{filename}'")
+            else:
+                response = requests.get(img_url, timeout=down_timeout)
                 with open(filename, "wb") as df:
                     df.write(response.content)
-                    print(f"[{i+1}/{t}] Downloaded '{url}' as '{filename}'")
-            except Exception as e:
-                failed.append(url)
-                print(f"[{i+1}/{t}] Failed to download '{url}', error: {e}")
-
-    if failed:
-        remaining = "\n".join(failed)
-        print(f"\nSome failed, please run again to download them: \n{remaining}")
-        with open(input_file, "w") as f:
-            f.write(remaining)
-        return False
-    else:
-        os.remove(input_file)
-        print(f"Successfully downloaded {dcnt} files, removed {input_file}")
-        return True
+                print(f"{progress} Downloaded '{img_url}' as '{filename}'")
+            
+            ctx.rec_downloaded(img_url)
+        except Exception as e:
+            print(f"{progress} Failed to download '{img_url}': {e}")
     
 
 def load_sites(file: str) -> list[str]:
@@ -178,33 +329,67 @@ def load_sites(file: str) -> list[str]:
         filtered = filter(lambda x : x and not x.startswith("#"), mapped)
         return list(filtered)
 
+
 def write_sites(file: str, sites: list[str]):
     with open(file, "w") as f:
         f.write("\n".join(sites))
+
+
+def preprocess_sites(ctx : Context):
+    if not ctx.pristine:
+        return
+
+    remaining = set(ctx.pristine)
+    for site in remaining:
+        try:
+            expanded = parse_pagination(site)
+            for s in expanded:
+                ctx.rec_preprocessed(s)
+        except Exception as e: 
+            print(f"Failed to parse pagination for '{site}', e: {e}")
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser(epilog="python3 buondua.py  -d '/my/folder' -m 'all' -f 'download_url.txt'",
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
     ap.add_argument("-d", "--dir", help="Download directory, by default it's current directory", required=False, default="./")
-    ap.add_argument("-f", "--file", help="Input file with all the website urls, by default it looks for 'download_url.txt'", required=False, default="download_url.txt")
+    ap.add_argument("-f", "--file", help="Input file with all the website urls, this argument is optional. By default it looks for 'download_url.txt'", required=False, default="download_url.txt")
 
     args = ap.parse_args()
     dir = args.dir if args.dir.endswith("/") else args.dir + "/"
 
     inputf = args.file
-    if not os.path.exists(parsed): 
-        if not os.path.exists(inputf):
-            open(inputf, "a").close() # touch empty file
-            print(f"Please provide website urls in {inputf}")
-            sys.exit(0)
 
-        sites = load_sites(inputf)
-        # print(f"Loaded sites: {sites}")
-        if not sites:
-            print(f"Found no website urls in {inputf}")
-            sys.exit(0)
-        parse_urls(sites, parsed, lambda sites: write_sites(inputf, sites))
+    if not os.path.exists(inputf):
+        open(inputf, "a").close() # touch empty file
+        print(f"Please provide website urls in '{inputf}'")
+        sys.exit(0)
 
-    if download(parsed, dir):
-        os.remove(inputf)
+    context = Context(inputf)
+    context.load()
+    
+    if context.is_finished():
+        print(f"Download is finished, remove '{inputf}' for another fresh download")
+        sys.exit(0)
+
+    preprocess_sites(context)
+    if len(context.pristine) > 0:
+        context.persist()
+        print(f"Failed to preprocess all websites, please try again")
+        sys.exit(0)
+
+    extract_img_urls(context)
+    if len(context.preprocessed) > 0:
+        context.persist()
+        print(f"Failed to parse all websites, please try again")
+        sys.exit(0)
+
+    download(context, dir)
+    if len(context.extracted) > 0:
+        context.persist()
+        print(f"Failed to download all images, please try again")
+        sys.exit(0)
+
+    context.persist()
+    print(f"Download is finished, remove '{inputf}' for another fresh download")
+
