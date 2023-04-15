a834c275c847edf97450cc6e7442889a98a94074
yongj.zhuang <zhuangyongj@gmail.com>
2023-04-16 01:26:56 +0800

    Modified:  ososk.py

--- ososk.py
+++ ososk.py
@@ -3,7 +3,7 @@ import os
 import time
 import sys
 import bs4
-from requests_html import HTMLSession
+from requests_html import HTMLSession, HTML
 
 '''
 # https://github.com/psf/requests-html/issues/341
@@ -20,13 +20,14 @@ async def browser(self):
 
 session = HTMLSession()
 
-def render(url: str, timeout = 20, retries = 2, wait = 3, sleep = 0) -> str:
+def render(url: str, request_timeout, render_timeout, retries, wait, sleep) -> str:
     start = time.monotonic_ns()
-    r = session.get(url, timeout=timeout)
+    r = session.get(url, timeout=request_timeout)
     print(f"Rquested '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
 
     start = time.monotonic_ns()
-    r.html.render(retries=retries, timeout=timeout, wait=wait, sleep=sleep)
+    rh: HTML = r.html
+    rh.render(retries=retries, timeout=render_timeout, wait=wait, sleep=sleep)
     print(f"Rendered '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
 
     return r.html.html
@@ -39,8 +40,13 @@ pip install requests_html
 if __name__ == "__main__":
 
     ap = argparse.ArgumentParser(description="ososk.py by yongjie.zhuang", formatter_class=argparse.RawTextHelpFormatter)
-    ap.add_argument('-f', '--file', type=str, help=f"File", required=False)
-    ap.add_argument('-u', '--url', action='append', type=str, help=f"Site url", required=False)
+    ap.add_argument('-f', '--file', type=str, help=f"input file with site urls", required=False)
+    ap.add_argument('-u', '--url', action='append', type=str, help=f"site url", required=False)
+    ap.add_argument('--retries', type=int, help=f"render retries, default 2", required=False, default=2)
+    ap.add_argument('--requesttimeout', type=float, help=f"request timeout in seconds, default 10s", required=False, default=10)
+    ap.add_argument('--rendertimeout', type=float, help=f"render timeout in seconds, default 60s", required=False, default=60)
+    ap.add_argument('--wait', type=float, help=f"time wait before rendering web page in seconds, default 3s", required=False, default=3)
+    ap.add_argument('--sleep', type=float, help=f"time sleep after initial render in seconds, default 0s", required=False, default=0)
     args = ap.parse_args()
 
     sites = []
@@ -54,7 +60,7 @@ if __name__ == "__main__":
         for au in args.url: sites.append(au)
 
     for url in sites:
-        html = render(url)
+        html = render(url, args.requesttimeout, args.rendertimeout, args.retries, args.wait, args.sleep)
         start = time.monotonic_ns()
         soup = bs4.BeautifulSoup(html, 'html.parser')
         print(f"BS4 parsed '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
@@ -62,29 +68,38 @@ if __name__ == "__main__":
         hrefs = []
         for a in soup.find_all('a', attrs={"data-fancybox" : "gallery"}):
             h = a.get('href')
-            print(h)
-            hrefs.append(h)
+            if h:
+                print(h)
+                r = h.rfind("-")
+                if r > -1:
+                    n: str = h[r+1:]
+                    n = n.replace("/", "-")
+                else:
+                    r = h.rfind("/")
+                    n = h[r+1:]
 
+                hrefs.append([h, n])
+            else:
+                imgs = a.findAll("img", recursive=False) 
+                for im in imgs: 
+                    h: str = im.get('src')
+                    nh = h
+                    q = nh.find("?")
+                    if q > -1: nh = nh[:q]
+                    n = nh[nh.rfind("/")+1:]
+                    hrefs.append([h, n])
 
-        for h in hrefs:
+        for h, n in hrefs:
             if not h: continue
 
-            r = h.rfind("-")
-            if r > -1:
-                filename: str = h[r+1:]
-                filename = filename.replace("/", "-")
-            else:
-                r = h.rfind("/")
-                filename = h[r+1:]
-
-            if os.path.exists(filename):
-                print(f"'{h}' already downloaded as '{filename}'")
+            if os.path.exists(n):
+                print(f"'{h}' already downloaded as '{n}'")
 
             try:
-                with open(filename, "wb") as df:
+                with open(n, "wb") as df:
                     start = time.monotonic_ns()
                     response = session.get(h, timeout=20)
                     df.write(response.content)
-                    print(f"Downloaded '{h}' as '{filename}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+                    print(f"Downloaded '{h}' as '{n}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
             except Exception as e:
                 print(f"Failed to download '{h}', error: {e}")
