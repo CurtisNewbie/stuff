341aefff8bf2f3760440c65fd27b2206b8c46469
yongj.zhuang <zhuangyongj@gmail.com>
2023-04-16 00:15:50 +0800

    Modified:  ososk.py

--- ososk.py
+++ ososk.py
@@ -1,5 +1,6 @@
+import argparse
 import os
-import time 
+import time
 import sys
 import bs4
 from requests_html import HTMLSession
@@ -27,6 +28,8 @@ def render(url: str, timeout = 20, retries = 2, wait = 3, sleep = 0) -> str:
     start = time.monotonic_ns()
     r.html.render(retries=retries, timeout=timeout, wait=wait, sleep=sleep)
     print(f"Rendered '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+
+    r.close()
     return r.html.html
 
 
@@ -35,46 +38,54 @@ pip install beautifulsoup4
 pip install requests_html
 '''
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    if len(argv) < 1:
-        raise ValueError("please specify path to the input file")
 
-    file = argv[0]
-    with open(file) as f:
-        all = f.read()
-        lines = all.splitlines()
-        for url in lines:
-            html = render(url)
-            start = time.monotonic_ns()
-            soup = bs4.BeautifulSoup(html, 'html.parser')
-            print(f"BS4 parsed '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+    ap = argparse.ArgumentParser(description="ososk.py by yongjie.zhuang", formatter_class=argparse.RawTextHelpFormatter)
+    ap.add_argument('-f', '--file', type=str, help=f"File", required=False)
+    ap.add_argument('-u', '--url', action='append', type=str, help=f"Site url", required=False)
+    args = ap.parse_args()
 
-            hrefs = []
-            for a in soup.find_all('a', attrs={"data-fancybox" : "gallery"}):
-                h = a.get('href')
-                print(h)
-                hrefs.append(h)
+    sites = []
+
+    if args.file:
+        with open(args.file) as f:
+            all = f.read()
+            sites = all.splitlines()
+
+    if args.url:
+        for au in args.url: sites.append(au)
+
+    for url in sites:
+        html = render(url)
+        start = time.monotonic_ns()
+        soup = bs4.BeautifulSoup(html, 'html.parser')
+        print(f"BS4 parsed '{url}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+
+        hrefs = []
+        for a in soup.find_all('a', attrs={"data-fancybox" : "gallery"}):
+            h = a.get('href')
+            print(h)
+            hrefs.append(h)
 
 
-            for h in hrefs:
-                if not h: continue
+        for h in hrefs:
+            if not h: continue
 
-                r = h.rfind("-")
-                if r > -1:
-                    filename: str = h[r+1:]
-                    filename = filename.replace("/", "-")
-                else:
-                    r = h.rfind("/")
-                    filename = h[r+1:]
+            r = h.rfind("-")
+            if r > -1:
+                filename: str = h[r+1:]
+                filename = filename.replace("/", "-")
+            else:
+                r = h.rfind("/")
+                filename = h[r+1:]
 
-                if os.path.exists(filename): 
-                    print(f"'{h}' already downloaded as '{filename}'")
+            if os.path.exists(filename):
+                print(f"'{h}' already downloaded as '{filename}'")
 
-                try:
-                    with open(filename, "wb") as df:
-                        start = time.monotonic_ns()
-                        response = session.get(h, timeout=20)
-                        df.write(response.content)
-                        print(f"Downloaded '{h}' as '{filename}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
-                except Exception as e:
-                    print(f"Failed to download '{h}', error: {e}")
+            try:
+                with open(filename, "wb") as df:
+                    start = time.monotonic_ns()
+                    response = session.get(h, timeout=20)
+                    df.write(response.content)
+                    print(f"Downloaded '{h}' as '{filename}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+            except Exception as e:
+                print(f"Failed to download '{h}', error: {e}")
