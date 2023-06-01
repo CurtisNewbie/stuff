a4c02acd86e516e992b562bf641da3e4bce715a6
yongj.zhuang <zhuangyongj@gmail.com>
2023-06-02 01:36:57 +0800

    Modified:  files_diff/files_diff.py

--- files_diff/files_diff.py
+++ files_diff/files_diff.py
@@ -19,17 +19,15 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("--fromdir", help="from dir", required=True, type=str)
     ap.add_argument("--todir", help="to dir", required=True, type=str)
+    ap.add_argument("--move-to", help="move non existing file to", required=False, type=str)
     args = ap.parse_args(sys.argv[1:])
 
     ff = list_files(args.fromdir)
     tf = list_files(args.todir)
 
     for p, ap in ff.items():
-        if p not in tf or ap['size'] != tf[p]['size']:
-            print(f"'{ap['abspath']}' not in '{args.todir}/'")
-
-    print("----------------------")
-
-    for p, ap in ff.items():
         if p in tf and ap['size'] == tf[p]['size']:
             print(f"'{ap['abspath']}' in '{args.todir}' as '{tf[p]['abspath']}' (size: {ap['size']})")
+            if args.moveto:
+                os.rename(ap['abspath'], f'{args.moveto}/{p}')
+                print(f"Moved '{ap['abspath']}' to f'{args.moveto}/{p}'")
