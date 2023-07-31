60913fcf8b30551daf01909df6f1935781a8ddc9
yongj.zhuang <yongj.zhuang@outlook.com>
2023-07-31 15:54:35 +0800

    Modified:  neovim/nvim/lua/file-finder.lua

--- neovim/nvim/lua/file-finder.lua
+++ neovim/nvim/lua/file-finder.lua
@@ -1,4 +1,4 @@
--- search files, even hidden ones
-vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files({no_ignore=true, hidden=true})<CR>', {})
+-- search files, respects gitignore
+vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files({no_ignore=true, hidden=false})<CR>', {})
 -- ripgrep files, respects gitignore
 vim.keymap.set('n', '<leader>gs', ':lua require"telescope.builtin".live_grep()<CR>', {})
