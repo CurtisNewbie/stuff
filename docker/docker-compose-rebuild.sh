435fe9373a7ee31feef980fc2366de029039afb2
yongj.zhuang <zhuangyongj@gmail.com>
2023-07-31 20:58:09 +0800

    Modified:  docker/docker-compose-rebuild.sh

--- docker/docker-compose-rebuild.sh
+++ docker/docker-compose-rebuild.sh
@@ -5,8 +5,8 @@ function rebuild () {
         echo "Must specify service name"
         return 1;
     fi
-    # docker-compose up -d --no-deps --build $1
-    docker-compose up -d --build $1 
+    # docker-compose up -d --no-deps --force --build $1
+    docker-compose up -d --build --foce $1 
 } 
 
 rebuild $1
