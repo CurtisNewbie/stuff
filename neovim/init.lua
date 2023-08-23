ffed5c6c44e1c432be800dfe93cbdb9daf2c3698
yongj.zhuang <yongj.zhuang@outlook.com>
2023-08-23 13:33:17 +0800

    Modified:  neovim/init.lua

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,10 +1,22 @@
-require('plugins')
-require('mason-config')
-require('lsp')
-require('code-completion')
-require('file-explorer')
-require('styling')
-require('file-finder')
-require('keymap')
+if vim.g.vscode then
+    -- https://github.com/sokhuong-uon/vscode-nvim/blob/main/init.lua
+    local file = {
+        formatDocument= function()
+            vim.fn.VSCodeNotify("editor.action.formatDocument")
+        end,
+    }
+    vim.keymap.set({'n'}, '==', file.formatDocument)
+else
+    require('plugins')
+    require('mason-config')
+    require('lsp')
+    require('code-completion')
+    require('file-explorer')
+    require('styling')
+    require('file-finder')
+    require('keymap')
+    vim.wo.number = true
+end
 
-vim.wo.number = true
+
+
