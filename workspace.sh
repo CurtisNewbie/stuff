7d123e8952f44b12c6f24a9a3746ddbbfde953f6
yongj.zhuang <yongj.zhuang@outlook.com>
2024-10-12 10:59:17 +0800

    Modified:  workspace.sh

--- workspace.sh
+++ workspace.sh
@@ -124,7 +124,7 @@ export LOC_BIN="/usr/local/bin"
 export USER_EXEC=~/exec
 
 # upgrade miso version
-miso_ver="v0.1.10"
+miso_ver="v0.1.11-beta.1"
 
 # github repo path: GIT_PATH
 # work repo path: WORK_REPO_PATH
