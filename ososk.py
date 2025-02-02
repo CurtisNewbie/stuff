59b51f21befa8c178033a0459569b9e12699e021
yongj.zhuang <yongj.zhuang@outlook.com>
2025-02-02 19:15:38 +0800

    Polishing
    Modified:  ososk.py

--- ososk.py
+++ ososk.py
@@ -1,12 +1,13 @@
 from curl_cffi import requests
 import bs4
 import argparse
-import os
+import os, sys
 import time
 
 ap = argparse.ArgumentParser(description="ososk.py 2.0 by yongjie.zhuang", formatter_class=argparse.RawTextHelpFormatter)
 ap.add_argument('-u', '--url', type=str, help=f"site url", required=True)
 ap.add_argument('--proxy', type=str, help=f"http(s) proxy server", required=False)
+ap.add_argument('--skip_mkdir', help=f"do not create new directory for the gallery", required=False, action="store_true", default=False)
 args = ap.parse_args()
 
 print(f"Parsing {args.url} using proxy: {args.proxy}")
@@ -19,12 +20,18 @@ html = requests.get(args.url, impersonate="chrome", proxies=proxies).text
 # print(html)
 
 soup = bs4.BeautifulSoup(html, 'html.parser')
+ttitle: "bs4.Tag" = soup.find("title")
+title = ttitle.text
+
+import re
+title = re.sub("[_ \-,.\(\)\']+", "_", title.strip())
+print(f"\n---\nTitle: '{title}'")
 
 hrefs = []
 for a in soup.find_all('a', attrs={"data-fancybox" : "gallery"}):
     h = a.get('href')
     if h:
-        print(h)
+        # print(h)
         r = h.rfind("-")
         if r > -1:
             n: str = h[r+1:]
@@ -48,18 +55,35 @@ print("\n----\nhtml parsed: ")
 for i, v in enumerate(hrefs):
     print(f"{i} - '{v}'")
 
+dir = title
+if not args.skip_mkdir:
+    try:
+        os.mkdir(dir)
+    except FileExistsError:
+        print(f"Directory '{dir}' already existed")
+        pass
+else:
+    dir = None
+
 print("\n----\ndownloading images: ")
 for h, n in hrefs:
     if not h: continue
-    if os.path.exists(n):
-        print(f"'{h}' already downloaded as '{n}'")
+    if dir is not None:
+        p = os.path.join(dir, n)
+    else:
+        p = n
+
+    if os.path.exists(p):
+        print(f"'{h}' already downloaded as '{p}'")
         continue
 
     try:
-        with open(n, "wb") as df:
+        with open(p, "wb") as df:
             start = time.monotonic_ns()
             response = requests.get(h, impersonate="chrome", timeout=20, proxies= proxies)
             df.write(response.content)
-            print(f"Downloaded '{h}' as '{n}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+            print(f"Downloaded '{h}' as '{p}' ({(time.monotonic_ns() - start) / 1e9:.3}s)")
+    except KeyboardInterrupt:
+        sys.exit(0)
     except Exception as e:
         print(f"Failed to download '{h}', error: {e}")
