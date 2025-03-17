77c49218ddb1ae630fdbde67a97cdca21efa61df
yongj.zhuang <yongj.zhuang@outlook.com>
2025-03-17 10:00:32 +0800

    Polishing
    Modified:  link_title.py

--- link_title.py
+++ link_title.py
@@ -4,25 +4,31 @@ from bs4 import BeautifulSoup
 
 import argparse
 ap = argparse.ArgumentParser(description="", formatter_class=argparse.RawTextHelpFormatter)
-ap.add_argument("-f", type=str, help=f"file", required=True)
+ap.add_argument("-f", type=str, help=f"file", required=False)
 args = ap.parse_args()
 
 file = args.f
-with open(file, "r") as f:
-    lines = f.readlines()
-    for l in lines:
-        l = l.strip()
-        if not l: continue
+if file:
+    with open(file, "r") as f:
+        content = f.read()
+else:
+    from pandas.io.clipboard import clipboard_get
+    content = clipboard_get()
 
-        try:
-            r = requests.get(l)
-            r.raise_for_status()
+lines = content.splitlines()
+for l in lines:
+    l = l.strip()
+    if not l: continue
 
-            title_tag = BeautifulSoup(r.content, 'html.parser').find('title')
-            title = ""
-            if title_tag:
-                title = title_tag.text.strip()
+    try:
+        r = requests.get(l)
+        r.raise_for_status()
 
-            print(f"- [{title}]({l})")
-        except Exception as e:
-            print(f"{l}, {e}")
+        title_tag = BeautifulSoup(r.content, 'html.parser').find('title')
+        title = ""
+        if title_tag:
+            title = title_tag.text.strip()
+
+        print(f"- [{title}]({l})")
+    except Exception as e:
+        print(f"{l}, {e}")
