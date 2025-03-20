f71f58e969af190192d18b32f78e6304ca7b6e54
yongj.zhuang <yongj.zhuang@outlook.com>
2025-03-20 10:25:13 +0800

    Polishing
    Modified:  release.py

--- release.py
+++ release.py
@@ -91,8 +91,10 @@ if __name__ == '__main__':
         last = current_tag()
         guessed = guess_next(last)
         print()
-        print(
-            f"Last release is '{last}'. Are you planning to release '{guessed}' ?")
+        if "beta" not in guessed:
+            print(f"Last release is '{last}'. Are you planning to release '{guessed}' or '{guessed}-beta.1' ?")
+        else:
+            print(f"Last release is '{last}'. Are you planning to release '{guessed}' ?")
         print()
         print(f"release {guessed}")
         print()
