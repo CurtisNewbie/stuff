046e35c30a474171343c13dcdfa5b95298ee4132
yongj.zhuang <yongj.zhuang@outlook.com>
2026-02-10 17:10:46 +0800

    Polishing

--- iflow/sync.sh
+++ iflow/sync.sh
@@ -12,5 +12,6 @@ echo "Synced .toml files from $SOURCE_DIR/commands to $COMMANDS_TARGET_DIR"
 # Sync skills
 SKILLS_TARGET_DIR="$HOME/.iflow/skills"
 mkdir -p "$SKILLS_TARGET_DIR"
+rm -rf "$SKILLS_TARGET_DIR"/*
 cp -r "$SOURCE_DIR/skills"/* "$SKILLS_TARGET_DIR/"
 echo "Synced skills files from $SOURCE_DIR/skills to $SKILLS_TARGET_DIR"
