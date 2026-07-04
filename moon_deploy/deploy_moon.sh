64deb6f712d4b0d6bb0f6b8d155cd27858d5633b
yongj.zhuang <yongj.zhuang@outlook.com>
2026-07-04 22:13:39 +0800

    Polishing

--- moon_deploy/deploy_moon.sh
+++ moon_deploy/deploy_moon.sh
@@ -6,6 +6,11 @@
     remote="alphaboi@curtisnewbie.com"
     remote_path="/home/alphaboi/services/nginx/html/bolobao/"
 
-    NODE_OPTIONS=--openssl-legacy-provider ng build --prod;
-    scp -r ./dist/moon/* "${remote}:${remote_path}"
+    NODE_OPTIONS=--openssl-legacy-provider ng build --prod
+
+    # Package dist as single archive (faster than scp -r of many files)
+    tar czf dist.tar.gz -C dist/moon .
+    scp dist.tar.gz "${remote}:${remote_path}"
+    ssh "${remote}" "cd ${remote_path} && tar xzf dist.tar.gz && rm dist.tar.gz"
+    rm dist.tar.gz
 )
